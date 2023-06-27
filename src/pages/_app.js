431eb58c63f2562e294c3e8d710a96252dd1104a
Seth Sun <sithsun@gmail.com>
2023-06-27 06:21:15 -0400

    added colors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,22 @@
-import '@/styles/globals.css'
-import {Montserrat} from 'next/font/google'
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  variable: '--font-mont'
-})
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-  <main className={`${montserrat.variable} font-mont`}>
-    <Component {...pageProps} />
-  </main>
-  )
-};
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={`${montserrat.variable} font-mont`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
